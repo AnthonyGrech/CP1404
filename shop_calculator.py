e668c36d205de44fc314ef4ce8a54afc81827751
AnthonyGrech <61459408+AnthonyGrech@users.noreply.github.com>
2020-08-31 12:31:00 +1000

    Add files via upload

---
+++ shop_calculator.py
@@ -0,0 +1,16 @@
+# Shop calculator program to determine total (discounted) price
+
+
+total = 0
+number = int(input("Number of items: "))
+while number < 0:
+    print("Invalid number of items!")
+    number = int(input("Number of items: "))
+for i in range(number):
+    price = float(input("Price of item: "))
+    total += price
+
+if total > 100:
+    total *= 0.9  # apply 10% discount
+
+print("Total price for ", number, " items is $", total, sep='')
